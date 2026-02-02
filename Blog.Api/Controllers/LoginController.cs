1f40c30c2a5c92caecfd01c1e3556a7992b4dab1
lewist4cj <lewist4cj@noreply.gitcode.com>
2026-02-02 12:54:01 +0800

    fix: sql synax err

--- Blog.Api/Controllers/LoginController.cs
+++ Blog.Api/Controllers/LoginController.cs
@@ -1,14 +1,19 @@
 using Blog.Common.Utils;
+using Blog.Services.Log;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers;
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class LoginController:BaseController
+public class LoginController(RuntimeLogService runtimeLogService) : BaseController
 {
+
     [HttpGet]
-    public IActionResult Login(HttpContext ctx)
+    public IActionResult Login()
     {
+        runtimeLogService.AddItemInfo("RunTimeLog","ce");
+        runtimeLogService.AddItemNowTime();
+        runtimeLogService.Save("Login");
         
         return Ok(new ApiResult<string>
         {
